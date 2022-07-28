7ef9dc45c35cab72fe06ddd62dea6f371f9f7c4f
Leonardo Santos <leonardojosantos@gmail.com>
2022-07-27 22:18:26 -0300

    Parte II

--- Projetos/stackList/src/Components/CardUser/index.jsx
+++ Projetos/stackList/src/Components/CardUser/index.jsx
@@ -3,10 +3,15 @@ import { Trash } from 'phosphor-react'
 import './style.css'
 
 export function CardUser(props) {
+    
+    function deleteUser(idUser) {
+        props.onDeleteStudent(idUser);
+    }
+
     return (
         <div className='container-card'>
             <p style={{color:'white'}}>{props.name}</p>
-            <button>
+            <button onClick={() => deleteUser(props.idStudent)}>
                 <Trash size={26} weight='bold' color='#fff' />
             </button>
         </div>
--- Projetos/stackList/src/Pages/Home/index.jsx
+++ Projetos/stackList/src/Pages/Home/index.jsx
@@ -1,24 +1,54 @@
 
+import { useState } from 'react'
 import './style.css'
 import { Plus } from 'phosphor-react'
 import { CardUser } from '../../Components/CardUser'
 
 export function Home() {
+    const [studentName, setStudentName] = useState('');
+    const [students, setStudents] = useState([]);
+
+    function handleAddStudent() {
+        if (studentName !== '') {
+            const newStudent = {
+                name: studentName
+            }
+
+            setStudents([...students, newStudent]);
+            // setStudents(prevState => [...prevState, newStudent]);
+
+            setStudentName('');
+        }
+        else {
+            alert('Para adicionar, digite um nome!😀') 
+        }
+    }
+
+    function handleRemoveStudent(idStudent) {
+        setStudents(students.filter((e, index) => index !== idStudent));
+    }
+
     return (
         <div className="container">
             <h1>Lista de alunos do Curso</h1>
 
             <div className="input-container">
-                <input type="text" placeholder="Nome do aluno" />
-                <button title="Adicionar aluno">
+                <input
+                    type="text"
+                    placeholder="Nome do aluno"
+                    onChange={e => setStudentName(e.target.value)}
+                    value={studentName}
+                />
+
+                <button title="Adicionar aluno" onClick={handleAddStudent}>
                     <Plus size={26} weight='bold' color='#fff' />
                 </button>
             </div>
 
             <div className="container-list">
-                <CardUser name="Leonardo Santos" />
-                <CardUser name="Leonardo Santos" />
-                <CardUser name="Leonardo Santos" />
+                {
+                    students.map((student, index) => <CardUser key={index} name={student.name} idStudent={index} onDeleteStudent={handleRemoveStudent} />)
+                }
             </div>
         </div>
     )
